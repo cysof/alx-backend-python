c3ae498f4583771379df8779e721b423c3b82712
cysof <cp4reallife@yahoo.com>
2024-08-23 20:43:51 +0100

    re vesit task 1

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-
-""" Module that contains type-annotated function that takes a string str1 and
-a string str2 as arguments and returns a concatenated string.
-"""
+''' Description: takes a string str1 and a string str2 as arguments
+    and returns a concatenated string
+        parameters: str1: str
+               str2: str
+'''
 
 
 def concat(str1: str, str2: str) -> str:
-    """Function to concatenate two strings and return the result.
-    """
+    ''' Return concatenation of str1 and str2. '''
     return str1 + str2
